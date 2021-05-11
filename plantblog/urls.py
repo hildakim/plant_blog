cba47c127c6fbc4915f35f5e9f6091a40e96bb49
hildakim <pinkredjohn@gmail.com>
2021-05-11 13:22:46 +0900

    startapp

--- plantblog/urls.py
+++ plantblog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('log/', include('log.urls')),
 ]
